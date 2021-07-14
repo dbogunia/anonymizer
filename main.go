039b8f89f86c0943e63894d243421669c106f6c1
Dominik Bogunia <dominik.bogunia@gmail.com>
2021-07-14 14:51:00 +0200

    empty

--- main.go
+++ main.go
@@ -125,7 +125,7 @@ func loadTablesFromFile() []string {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	// optionally, resize scanner's capacity for lines over 64K, see next example
+	
 	for scanner.Scan() {
 		fmt.Println(scanner.Text())
 		tables = append(tables, scanner.Text())
